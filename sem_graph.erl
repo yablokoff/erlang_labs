23c92102f346691f375118debf910b0cef5b8a88
Yablokoff <yablokoff@yablokoff-desktop.(none)>
2012-03-16 21:09:08 +0400

    Parent's children's links added

--- sem_graph.erl
+++ sem_graph.erl
@@ -12,14 +12,29 @@ map(F, X, [H|Tail]) ->
 	NewList = map(F, X, Tail),
 	[F(H,X)|NewList].
 
-findFacts(Obj, Db) ->
-	Relations = [ {Link, Subject} || {Object, Link, Subject} <- Db, Object == Obj ],
-	InclusionRelations = [ Subject || {Link, Subject} <- Relations, Link == is ],
-	NewRelations = map(fun findFacts/2, Db, InclusionRelations),
+findParents(Obj, Db) ->
+	%
+	% Make list of "is-parents" for Obj
+	%
+	Relations = [ Subject || {Object, Link, Subject} <- Db, Object == Obj, Link == is ],
+	NewRelations = map(fun findParents/2, Db, Relations),
+	lists:flatten(lists:merge(Relations, NewRelations)).
+	
+findChildren(Subj, Db) ->
+	%
+	% Make list of "is-children" for Subj
+	%
+	Relations = [ Object || {Object, Link, Subject} <- Db, Subject == Subj, Link == is ],
+	NewRelations = map(fun findChildren/2, Db, Relations),
 	lists:flatten(lists:merge(Relations, NewRelations)).
 
-findLinks()
-	->
+findSpecs(Obj, Db) ->
+	%
+	% Find all first-level specs for Obj
+	%
+	[ {Object, Link, Subject} || {Object, Link, Subject} <- Db, Object == Obj ].
+
+findLinks()	->
 	Db = [{avtoVAZ, is, auto_producer}, {priora, produced_by, avtoVAZ}, {priora, presented_in, 2007},
 			{priora, is, passenger_car}, {kalina, produced_by, avtoVAZ}, {kalina, presented_in, 2004},
 			{kalina, is, passenger_car}, {passenger_car, has_max_engine_volume, three_l}, {passenger_car, has_max_price, 25000},
@@ -27,16 +42,19 @@ findLinks()
 			{the_biggest_steel_consumer, is, avtoVAZ}
 		 ],
 	{ok,Obj} = io:read("Set object: "),
-	AllFacts = findFacts(Obj,Db),
-	Subjs = [{Object, Link} || {Object, Link, Subject} <- Db, Subject == Obj],
+	Parents = findParents(Obj,Db),
+	ParentsFacts = lists:flatten(map(fun findSpecs/2, Db, Parents)),
+	ChildrenAux = findChildren(Obj,Db),
+	Children = [Obj|ChildrenAux], % add object to list of children so that show object's properties
+	ChildrenFacts = lists:flatten(map(fun findSpecs/2, Db, Children)),
 	{ok,L} = io:read("Set link: "),
 	Links = [{Object, Link, Subject} || {Object, Link, Subject} <- Db, Link == L],
-	if AllFacts =/= [] ->
-		io:fwrite("Facts with object : ~W~n", [AllFacts,19]);
+	if ParentsFacts =/= [] ->
+		io:fwrite("Parents' facts: ~W~n", [ParentsFacts,19]);
 		true -> empty
 	end,
-	if Subjs =/= [] ->
-		io:fwrite("Facts with subject : ~W~n", [Subjs,19]);
+	if ChildrenFacts =/= [] ->
+		io:fwrite("Object's and children' facts: ~W~n", [ChildrenFacts,19]);
 		true -> empty
 	end,
 	if Links =/= [] ->
